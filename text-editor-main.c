d40f51f12d069141dbf76ba32daf985f05c20f1e
gmicros <gmicros@localhost.localdomain>
2021-01-04 17:06:50 -0500

    debug window
    Create window in the bottom right to print debug messages

--- text-editor-main.c
+++ text-editor-main.c
@@ -4,13 +4,32 @@
 
 #include <ncurses.h>
 
+WINDOW *debug_win;
+
+void print_debug(char* msg){
+	werase(debug_win);
+	wprintw(debug_win, "%s", msg);
+	wrefresh(debug_win);
+	
+}
+
 int main() {
 	int ch;
 	initscr();		// Start curses mode
 	raw();			// line buffering disabled
 	keypad(stdscr, TRUE);	// get arrows and F-keys
 	noecho();		// dont echo while we getch
-
+	
+	// debug output window
+	int startx, starty, height, width;
+	height = 3;
+	width = 10;
+	starty = LINES-height;
+	startx = COLS - width;
+	debug_win = newwin(height, width, starty, startx);
+	box(debug_win, 0, 0);
+	wrefresh(debug_win);
+	
 	printw("Text editor:\n");
 	refresh();
 	
@@ -23,17 +42,21 @@ int main() {
 				int y, x;
 				getyx(stdscr,y,x);
 				mvdelch(y,x-1);
-
+				print_debug("BACKSPACE");
 				// TODO(gmicros): handle deleting a line
 				break;
 			}
 			// ENTER
 			case 10:
 				printw("\n");
+				print_debug("ENTER");
 				break;
 			// REGULAR CHARACTER
 			default:
 				printw("%c", ch);
+				char str[30];
+				sprintf(str, "input: %c\n", ch);
+				print_debug(str);
 	
 		}
 
